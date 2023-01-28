9999be5d8d52154d04efca31f830080198e26436
Daniel Chukwudubem <princedaniel7890@gmail.com>
2023-01-28 16:04:19 +0100

    ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-
-      
+      colors: {
+         MarineBlue:" hsl(213, 96%, 18%)",
+         PurplishBlue: "hsl(243, 100%, 62%)",
+         PastelBlue: "hsl(228, 100%, 84%)",
+         LightBlue: "hsl(206, 94%, 87%)",
+         StrawberryRed:" hsl(354, 84%, 57%)",
+         CoolGray: "hsl(231, 11%, 63%)",
+         LightGray:" hsl(229, 24%, 87%)",
+         Magnolia:" hsl(217, 100%, 97%)",
+         Alabaster:" hsl(231, 100%, 99%)",
+         White: "hsl(0, 0%, 100%)",
+      }
     },
   },
   plugins: [
