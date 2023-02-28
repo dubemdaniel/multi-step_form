2337f44063924e2364ddef4738b5ed4db4b197a6
Daniel Chukwudubem <princedaniel7890@gmail.com>
2023-02-28 09:46:54 +0100

    started js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// comment
+const next = document.getElementById('next_btn')
+const back = document.getElementById('back_btn')
+const wrapper = document.querySelectorAll('.wrapper')
+let current = 0
+next.addEventListener('click', () => {
+    if(current < 4){
+        wrapper[current].classList.add('hidden');
+        wrapper[current + 1].classList.remove('hidden')
+        current++
+            if(current===3){
+                next.innerHTML = ''
+                next.innerHTML = `
+                <button class="px-3 py-2 mr-2 font-medium text-white rounded-md text-md bg-MarineBlue md:px-1 md:py-1 md:text-xl" id="next_btn" type="submit"> Confirm </button>
+                `
+            }
+         if(current===4){
+            next.classList.add('hidden')
+        }
+    }
+})
