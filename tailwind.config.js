b1f75ef1ba0df534c704550559483489ac1d5fc2
kingsley-sunny <ezeobisunny51@gmail.com>
2023-03-30 20:05:02 +0100

    [corrections]: fixed the background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "*.{html,js}"
-  ],
+  content: ["*.{html,js}"],
   theme: {
     extend: {
       colors: {
-         MarineBlue:" hsl(213, 96%, 18%)",
-         PurplishBlue: "hsl(243, 100%, 62%)",
-         PastelBlue: "hsl(228, 100%, 84%)",
-         LightBlue: "hsl(206, 94%, 87%)",
-         StrawberryRed:" hsl(354, 84%, 57%)",
-         CoolGray: "hsl(231, 11%, 63%)",
-         LightGray:" hsl(229, 24%, 87%)",
-         Magnolia:" hsl(217, 100%, 97%)",
-         Alabaster:" hsl(231, 100%, 99%)",
-         White: "hsl(0, 0%, 100%)",
+        MarineBlue: " hsl(213, 96%, 18%)",
+        PurplishBlue: "hsl(243, 100%, 62%)",
+        PastelBlue: "hsl(228, 100%, 84%)",
+        LightBlue: "hsl(206, 94%, 87%)",
+        StrawberryRed: " hsl(354, 84%, 57%)",
+        CoolGray: "hsl(231, 11%, 63%)",
+        LightGray: " hsl(229, 24%, 87%)",
+        Magnolia: " hsl(217, 100%, 97%)",
+        Alabaster: " hsl(231, 100%, 99%)",
+        White: "hsl(0, 0%, 100%)",
       },
-      backgroundImage:{
-        desktopImg:"url(/assets/images/bg-sidebar-desktop.svg)",
-        mobileImg:"url(/assets/images/bg-sidebar-mobile.svg);"
+      backgroundImage: {
+        desktopImg: "url('/assets/images/bg-sidebar-desktop.svg')",
+        mobileImg: "url('/assets/images/bg-sidebar-mobile.svg');",
       },
       fontFamily: {
-        myFont: ['Montserrat' , 'sans-serif']
+        myFont: ["Montserrat", "sans-serif"],
       },
-      image:{
-        advanced:"url(/assets/images/icon-advanced.svg)",
-        arcade:"url(/assets/images/icon-arcade.svg)",
-        pro:"url(/assets/images/icon-pro.svg)",
-        thanks:"url(/assets/images/icon-thank-you.svg)",
-        advanced:"url(/assets/images/icon-advanced.svg)",
+      image: {
+        advanced: "url(/assets/images/icon-advanced.svg)",
+        arcade: "url(/assets/images/icon-arcade.svg)",
+        pro: "url(/assets/images/icon-pro.svg)",
+        thanks: "url(/assets/images/icon-thank-you.svg)",
+        advanced: "url(/assets/images/icon-advanced.svg)",
       },
-      boxShadow:{
-        myShadow:"-1px -1px 10px hsla(213, 96%, 18%, 0.2)"
-      }
+      boxShadow: {
+        myShadow: "-1px -1px 10px hsla(213, 96%, 18%, 0.2)",
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
